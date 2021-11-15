47ab7300e4466cbd1b9805613cc5f398fbc50ab2
DJNK0 <65711556+DJNK0@users.noreply.github.com>
2021-11-15 09:31:42 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,56 @@
+morse = {
+  "a":'.-',
+  "b":'-...',
+  "c":'-.-.',
+  "d":'-..',
+  "e":'.',
+  "f":'..-.',
+  "g":'--.',
+  "h":'....',
+  "i":'..',
+  "j":'.---',
+  "k":'-.-',
+  "l":'.-..',
+  "m":'--',
+  "n":'-.',
+  "o":'---',
+  "p":'.--.',
+  "q":'--.-',
+  "r":'.-.',
+  "s":'...',
+  "t":'-',
+  "u":'..-',
+  "v":'...-',
+  "w":'.--',
+  "x":'-..-',
+  "z":'..--'
+}
+
+def encrypt(string):
+  string = string.lower()
+  answer = ''
+  for char in string:
+    if char == ' ':
+      answer += '\\'
+    if char.isalpha(): 
+      answer += morse[char] + " "
+    else:
+      answer += char
+  return answer
+
+def decrypt(string):
+  answer = ' '
+  reverse_morse = {value: key for (key, value) in morse.items()}
+  string = string.split(' ')
+
+  for char in string:
+    if char == "\\":
+      answer += ' '
+    elif char != '':
+      answer += reverse_morse[char]
+    else:
+      answer += ' '
+  return answer
+    
+string = '-.. . ..-- . \ .-.. . ... \ .. ... \ ..-- . . .-. \ .. -. - . .-. . ... .- -. -'
+print((decrypt(string)))
